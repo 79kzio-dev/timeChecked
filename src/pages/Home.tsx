96a80fe0e6c7e5a52b8ef18be6071a24685406ae
kzio <kzio79@naver.com>
2026-07-30 05:55:03 +0900

    14th commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,197 +1,201 @@
-import {Box, Container, Paper, Typography} from "@mui/material";
+import { Box, Container, Paper, Typography } from "@mui/material";
 
 import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
 
 import MenuCard from "../components/MenuCard";
-import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-    const [currentTime, setCurrentTime] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
 
-    useEffect(() => {
-        const updateTime = () => {
-            const now = new Date();
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
 
-            const date = now.toLocaleDateString("ko-KR", {
-                year: "numeric",
-                month: "2-digit",
-                day: "2-digit",
-                weekday: "short"
-            });
+      const date = now.toLocaleDateString("ko-KR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        weekday: "short"
+      });
 
-            const time = now.toLocaleTimeString("ko-KR", {
-                hour: "2-digit",
-                minute: "2-digit",
-                hour12: false
-            });
+      const time = now.toLocaleTimeString("ko-KR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+      });
 
-            setCurrentTime(`${date} ${time}`);
-        };
+      setCurrentTime(`${date} ${time}`);
+    };
 
-        updateTime();
+    updateTime();
 
-        const timer = setInterval(updateTime, 1000);
+    const timer = setInterval(updateTime, 1000);
 
-        return () => clearInterval(timer);
-    }, []);
+    return () => clearInterval(timer);
+  }, []);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <Container
-            maxWidth="sm"
-            sx={{
-                py: 3,
-            }}
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        py: 3
+      }}
+    >
+      {/* Header */}
+      <Paper
+        elevation={0}
+        onClick={() => navigate("/")}
+        sx={{
+          p: 3,
+          mb: 3,
+          borderRadius: 4,
+          bgcolor: "primary.main",
+          color: "primary.contrastText",
+          position: "sticky",
+          top: 0,
+          zIndex: 1000,
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 0
+          }}
         >
-            {/* Header */}
-            <Paper
-                elevation={0}
-                onClick={() => navigate("/")}
-                sx={{
-                    p: 3,
-                    mb: 3,
-                    borderRadius: 4,
-                    bgcolor: "primary.main",
-                    color: "primary.contrastText",
-                }}
-            >
-                <Box
-                    sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        gap: 1,
-                    }}
-                >
-                    <VerifiedUserIcon fontSize="medium"/>
+          <VerifiedUserIcon fontSize="medium" />
 
-                    <Typography
-                        variant="h6"
-                        sx={{
-                            fontWeight: 700,
-                        }}
-                    >
-                        The Guardians 강남점
-                        <br/> 시설물 점검 관리
-                    </Typography>
-                </Box>
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: 700
+            }}
+          >
+            The Guardians 강남점 시설물 점검 관리
+          </Typography>
+        </Box>
 
-                <Typography
-                    sx={{
-                        mt: 1,
-                        textAlign: "end",
-                        opacity: 0.9,
-                        fontSize: 15,
-                    }}
-                >
-                    {currentTime}
-                </Typography>
-            </Paper>
+        <Typography
+          sx={{
+            mt: 1,
+            textAlign: "end",
+            opacity: 0.9,
+            fontSize: 15
+          }}
+        >
+          {currentTime}
+        </Typography>
+      </Paper>
 
-            <MenuCard
-                title="본관 종합점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Mb",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="신관 종합점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Nb",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="HOS 종합점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Hos",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="식품관 점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Food",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="스위트 파크 점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Sweet",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="외곽 및 점외 시설물 점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Out",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="기술 점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Tech",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="보조 배터리 점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Battery",
-                        },
-                    })
-                }
-            />
-            <MenuCard
-                title="누수 점검"
-                onClick={() =>
-                    navigate("/worker", {
-                        state: {
-                            type: "Water",
-                        },
-                    })
-                }
-            />
-            <MenuCard title="메모G" onClick={() => navigate("/memo", {})}/>
+      <MenuCard
+        title="본관 종합점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Mb"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="신관 종합점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Nb"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="HOS 종합점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Hos"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="식품관 점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Food"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="스위트 파크 점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Sweet"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="외곽 및 점외 시설물 점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Out"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="기술 점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Tech"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="보조 배터리 점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Battery"
+            }
+          })
+        }
+      />
+      <MenuCard
+        title="누수 점검"
+        onClick={() =>
+          navigate("/worker", {
+            state: {
+              type: "Water"
+            }
+          })
+        }
+      />
+      <MenuCard title="메모G" onClick={() => navigate("/memo", {})} />
 
-            <Typography
-                sx={{
-                    mt: 4,
-                    textAlign: "end",
-                    color: "text.secondary",
-                    fontSize: 14,
-                }}
-            >
-                Developed by Kzio
-            </Typography>
-        </Container>
-    );
+      <Typography
+        sx={{
+          mt: 4,
+          textAlign: "end",
+          color: "text.secondary",
+          fontSize: 14
+        }}
+      >
+        Developed by Kzio
+      </Typography>
+    </Container>
+  );
 }
